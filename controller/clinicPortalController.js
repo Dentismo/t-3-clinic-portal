71ec21dcde9ec444167e891f9bcc625d71e50b91
ansis <gusplean@student.gu.se>
2023-01-06 19:08:38 +0200

    Fix bookings not displaying

--- controller/clinicPortalController.js
+++ controller/clinicPortalController.js
@@ -1,69 +1,57 @@
-const Dentist = require('../models/dentist.js');
-const Clinic = require('../models/clinic.js');
-const mongoose = require('mongoose');
+const Dentist = require("../models/dentist.js");
+const Clinic = require("../models/clinic.js");
+const mongoose = require("mongoose");
 
 class ClinicPortalController {
+  constructor() {}
 
-    constructor() {}
+  // Returns the specific dentist
+  async getDentist(id) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id) || id === null)
+        return { message: "ID is not valid for given request" };
+      const dentist = await Dentist.findById(id);
+      if (!dentist)
+        return { message: "Dentist could not be found with given ID" };
 
-    // Returns the specific dentist
-    async getDentist(id) {
-        try {
-            if (!mongoose.Types.ObjectId.isValid(id) || id === null) 
-                return {message: 'ID is not valid for given request'};
-
-            const dentist = await Dentist.findById(id);  
-
-            if (!dentist)
-                return {message: 'Dentist could not be found with given ID'};
-
-            return JSON.stringify(dentist);
-
-        } catch (error) {
-            console.log(error)
-            return {message: 'Dentist could not be found with given ID'};
-        }
-        
+      return JSON.stringify(dentist);
+    } catch (error) {
+      console.log(error);
+      return { message: "Dentist could not be found with given ID" };
     }
+  }
 
-    // Returns the specific clinic's information
-    async getClinic(messageBody) {
-        try {
-            const { _id } = messageBody
+  // Returns the specific clinic's information
+  async getClinic(messageBody) {
+    try {
+      const { _id } = messageBody;
 
-            console.log(`\n ${_id} \n`)
+      if (!mongoose.Types.ObjectId.isValid(_id) || _id === null)
+        return { message: "ID is not valid for given request" };
+      const clinic = await Clinic.findById(_id);
+      if (!clinic)
+        return { message: "Clinic could not be found with given ID" };
 
-            if (!mongoose.Types.ObjectId.isValid(_id) || _id === null) 
-                return {message: 'ID is not valid for given request'};
-
-            const clinic = await Clinic.findById(_id); 
-
-            if (!clinic)
-                return {message: 'Clinic could not be found with given ID'};
-
-            return clinic;
-
-        } catch (error) {
-            console.log(error)
-            return {message: 'Clinic could not be found with given ID'};
-        }
+      return clinic;
+    } catch (error) {
+      console.log(error);
+      return { message: "Clinic could not be found with given ID" };
     }
+  }
 
-    // Returns the list of all clinics
-    async getClinics() {
-        try {
-            const clinics = await Clinic.find({}); 
+  // Returns the list of all clinics
+  async getClinics() {
+    try {
+      const clinics = await Clinic.find({});
 
-            if (!clinics)
-                return {message: 'Clinics were not found!'};
+      if (!clinics) return { message: "Clinics were not found!" };
 
-            return JSON.stringify(clinics);
-
-        } catch (error) {
-            console.log(error)
-            return {message: 'Clinics could not be found'};
-        }
+      return JSON.stringify(clinics);
+    } catch (error) {
+      console.log(error);
+      return { message: "Clinics could not be found" };
     }
+  }
 }
 
-module.exports = ClinicPortalController
+module.exports = ClinicPortalController;
--- controller/mqtt-handler.js
+++ controller/mqtt-handler.js
@@ -1,5 +1,5 @@
 const mqtt = require("mqtt");
-const ClinicPortalController = require("./clinicPortalController")
+const ClinicPortalController = require("./clinicPortalController");
 
 const clinic = new ClinicPortalController();
 
@@ -20,7 +20,10 @@ class MqttHandler {
 
   connect() {
     // Connect mqtt with credentials (in case of needed, otherwise we can omit 2nd param)
-    this.mqttClient = mqtt.connect(this.host, { username: this.username, password: this.password });
+    this.mqttClient = mqtt.connect(this.host, {
+      username: this.username,
+      password: this.password,
+    });
 
     // Mqtt error calback
     this.mqttClient.on("error", (err) => {
@@ -30,46 +33,50 @@ class MqttHandler {
 
     // Connection callback
     this.mqttClient.on("connect", () => {
-      console.log(`mqtt client connected, Subscribed to ${this.requestDentistTopic}\n`);
-      console.log(`mqtt client connected, Subscribed to ${this.requestClinicTopic}\n`);
-      console.log(`mqtt client connected, Subscribed to ${this.requestClinicsTopic}`);
-      this.mqttClient.subscribe(this.requestDentistTopic, {qos: 1});
-      this.mqttClient.subscribe(this.requestClinicTopic, {qos: 1});
-      this.mqttClient.subscribe(this.requestClinicsTopic, {qos: 1});
+      console.log(
+        `mqtt client connected, Subscribed to ${this.requestDentistTopic}\n`
+      );
+      console.log(
+        `mqtt client connected, Subscribed to ${this.requestClinicTopic}\n`
+      );
+      console.log(
+        `mqtt client connected, Subscribed to ${this.requestClinicsTopic}`
+      );
+      this.mqttClient.subscribe(this.requestDentistTopic, { qos: 1 });
+      this.mqttClient.subscribe(this.requestClinicTopic, { qos: 1 });
+      this.mqttClient.subscribe(this.requestClinicsTopic, { qos: 1 });
     });
 
-    
     const client = this.mqttClient;
     // When a message arrives, console.log it
     this.mqttClient.on("message", async function (topic, message) {
-
-       //-------------------------------------------------------------------\\
-      let incomingTopic = topic.split("/") // array of topic fields
-      const id = incomingTopic[2]  //   [request, clinicportal, clinic, id]
-      incomingTopic.splice(2,1) // removes id = [request, clinicportal, clinic]
-      const finalTopic = incomingTopic.join("/") // finalTopic = request/clinicportal/clinic 
-       //--------------------------------------------------------------------\\
-
-       console.log(incomingTopic, message.toString())
-
+      //-------------------------------------------------------------------\\
+      let incomingTopic = topic.split("/"); // array of topic fields
+      const id = incomingTopic[2]; //   [request, clinicportal, clinic, id]
+      incomingTopic.splice(2, 1); // removes id = [request, clinicportal, clinic]
+      const finalTopic = incomingTopic.join("/"); // finalTopic = request/clinicportal/clinic
+      //--------------------------------------------------------------------\\
       switch (finalTopic) {
         case "request/dentist":
-          const responseDentist = await clinic.getDentist(message.toString());
+          const responseDentist = await clinic.getDentist(
+            JSON.parse(message.toString())._id
+          );
           client.publish(`response/dentist/${id}`, responseDentist);
-          console.log(responseDentist);
           break;
-        
+
         case "request/clinic":
-          console.log(message.toString())
-          const responseClinic = await clinic.getClinic(JSON.parse(message.toString()));
-          client.publish(`response/clinic/${id}`, JSON.stringify(responseClinic));
-          console.log(responseClinic);
+          const responseClinic = await clinic.getClinic(
+            JSON.parse(message.toString())
+          );
+          client.publish(
+            `response/clinic/${id}`,
+            JSON.stringify(responseClinic)
+          );
           break;
 
         case "request/clinics":
           const responseClinics = await clinic.getClinics();
           client.publish(`response/clinics/${id}`, responseClinics);
-          console.log(responseClinics);
           break;
       }
     });
